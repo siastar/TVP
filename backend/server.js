399d1366c6f3077606598c40a21b6e9618b8f49c
siastar <info@siastar.me>
2020-01-16 01:05:07 +0100

    added test fot GET and POST

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,42 @@
 console.log('node now running...');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000
 
+//middlewares\\
+
+app.use(bodyParser.urlencoded({extended : true}));
+//https://www.npmjs.com/package/body-parser
+
 app.get('/test', (req, res) => {
-    res.send('Express server running');
-});
-
-
+    res.send('Express server running - this message is hardcoded in server.js');
+});//just a test route
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/server.html')
+    //__dirname is the project root folder (the one with package.json)
 })
 
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
+//when data provided through server.html forms returns something like
+// {
+//     name: 'Yoda',
+//     quote: 'Gotta green big ears'
+// }
+
+app.post('/records', (req, res) => {
+  console.log(req.body)
+})
+//when data provided through server.html forms returns something like
+// {
+//   artist: 'Ozric Tentacles',
+//   title: 'Pungent Effulgent',
+//   year: '1989'
+// }
 
 app.listen(PORT, () => {
     console.log('server listening on port' , PORT);
